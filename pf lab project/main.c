17f23917df384a762d1ee08f3c2c55f393ecd1c8
saahil004 <165182276+saahil004@users.noreply.github.com>
2024-11-22 15:54:54 +0500

    Update main.c

--- pf lab project/main.c
+++ pf lab project/main.c
@@ -3,6 +3,12 @@
 #include <string.h>
 #include <unistd.h>
 
+struct billItem
+{
+    char name[20];
+    int qty;
+};
+
 struct item* getItems();
 int logIn();
 void showItems(struct item *arr, int size);
@@ -21,7 +27,6 @@ struct item
     char type[10];
     int qty;
     int price;
-    // struct supplier supplier;
 };
 
 
@@ -116,8 +121,116 @@ int logIn()
     return 0;
 }
 
+void Billing(struct item *arr, int size) {
+    int choice = 1;
+    int qty;
+    int bill = 0;
+    int amount;
+    int change = 0;
+    struct billItem bi[size];
+    for (int i = 0; i < size; i++)
+    {
+       strcpy(bi[i].name, " "); 
+       bi[i].qty = 0; 
+    }
+    while (choice > 0 && choice <= size)
+    {
+        showItems(arr, size);
+        printf("Enter item number or any invalid number to exit: ");
+        scanf("%d", &choice);
+        if (choice <= 0 || choice > size)
+        {
+            break;
+        }
+        
+        printf("Enter qty: ");
+        scanf("%d", &qty);
+        if (arr[choice-1].qty >= qty)
+        {
+            bill += qty * arr[choice - 1].price;
+            printf("Item added.\n");
+            strcpy(bi[choice-1].name, arr[choice-1].name);
+            bi[choice-1].qty = qty;
+            arr[choice - 1].qty -= qty;
+            sleep(2);
+        } else {
+            printf("This quantity not available.\n");
+            sleep(2);
+        }
+        system("cls");
+
+        
+    }
+    system("cls");
+    printf("Your Bill:\n");
+    for (int i = 0; i < size; i++)
+    {
+        if (bi[i].qty > 0)
+        {
+            printf("%d  %s: %d\n", bi[i].qty, bi[i].name, arr[i].price * bi[i].qty);
+        }
+        
+    }
+    printf("Total: %d\n", bill);
+    pay:
+    printf("Enter amount paid: ");
+    scanf("%d", &amount);
+    if (amount < bill)
+    {
+        goto pay;
+    } else {
+        if (amount == bill)
+        {
+            printf("Paid.\n");
+        } else {
+            change = amount - bill;
+            printf("Paid... change: %d\n", change);
+        }
+        
+    }
+    
+    FILE *fi = fopen("items.txt", "w");
+    fprintf(fi, "%d\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(fi, "%s\n", arr[i].name);
+        fprintf(fi, "%s\n", arr[i].type);
+        fprintf(fi, "%d\n", arr[i].qty);
+        fprintf(fi, "%d\n", arr[i].price);
+    }
+    fclose(fi);
+    
+    
+    
+}
+
+void reorder(struct item *arr, int size) {
+    int reorderlvl = 50;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i].qty <= reorderlvl)
+        {
+            arr[i].qty += 50;
+            printf("ordering %s from supplier....\n", arr[i].name);
+        }
+        
+    }
+    FILE *fr = fopen("items.txt", "w");
+    fprintf(fr, "%d\n", size);
+    for (int i = 0; i < size; i++)
+    {
+        fprintf(fr, "%s\n", arr[i].name);
+        fprintf(fr, "%s\n", arr[i].type);
+        fprintf(fr, "%d\n", arr[i].qty);
+        fprintf(fr, "%d\n", arr[i].price);
+    }
+    fclose(fr);
+    
+}
+
 int main()
 {
+    int cont = 1;
     int f = 0;
     while (f == 0)
     {
@@ -131,8 +244,23 @@ int main()
     int size;
     FILE *fit = fopen("items.txt", "r");
     fscanf(fit, "%d", &size);
+    fclose(fit);
     // printf("%d", size);
-    printf("The items that are available in our supermarket: \n");
-    showItems(items, size);
+    // printf("The items that are available in our supermarket: \n");
+    // showItems(items, size);
+    again:
+    Billing(items, size);
+    printf("Checking for need of reordering from supplier and updating...\n");
+    reorder(items, size);
+    sleep(8);
+    system("cls");
+    printf("Next order? (enter 1 for more orders or 0 to sign out)\n");
+    // int cont;
+    scanf("%d", &cont);
+    if (cont == 1) {
+        goto again;
+    }
+    printf("Signing out...");
+    sleep(3);
     return 0;
 }
