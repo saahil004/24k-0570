b90b1a55739b972d9b5f6cb751bdc2ebda3785ec
saahil004 <165182276+saahil004@users.noreply.github.com>
2024-11-22 18:34:57 +0500

    Create admin.c

---
+++ pf lab project/admin.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+#include <unistd.h>
+#include <unistd.h>
+
+struct admin
+{
+    char user[20];
+    char pass[20];
+    
+};
+
+struct item
+{
+    char name[20];
+    char type[10];
+    int qty;
+    int price;
+};
+
+
+int login();
+void newItem();
+void newEmployee();
+void newAdmin();
+
+void newItem() {
+    struct item newItem;
+    int noOfItems;
+    char name[20], type[20];
+    int qty, price;
+    printf("Enter name of the item: ");
+    scanf("%19s", name);
+    printf("Enter type of the item: ");
+    scanf("%19s", type);
+    printf("Enter quantity of the item: ");
+    scanf("%d", &qty);
+    printf("Enter price of the item: ");
+    scanf("%d", &price);
+    FILE *fi = fopen("items.txt", "r");
+    fscanf(fi, "%d", &noOfItems);
+    fclose(fi);
+    FILE *fip = freopen("items.txt", "r+", fi);
+    fprintf(fip, "%d\n", noOfItems + 1);
+    fclose(fip);
+    FILE *fp = freopen("items.txt", "a", fi);
+    fprintf(fp, "%s\n", name);
+    fprintf(fp, "%s\n", type);
+    fprintf(fp, "%d\n", qty);
+    fprintf(fp, "%d\n", price);
+}
+
+
+
+int login() {
+    FILE *fad = fopen("admin.txt", "r");
+    int noOfAdmins;
+    fscanf(fad, "%d", &noOfAdmins);
+    // printf("%d", noOfAdmins);
+    char user[20];
+    char pass[20];
+    struct admin admins[noOfAdmins];
+    for (int i = 0; i < noOfAdmins; i++)
+    {
+        fscanf(fad, "%19s", user);
+        fscanf(fad, "%19s", pass);
+        for (int j = 0; j < strlen(user); j++)
+        {
+            user[j] = user[j] ^ 22;
+        }
+        for (int j = 0; j < strlen(pass); j++)
+        {
+            pass[j] = pass[j] ^ 22;
+        }
+        strcpy(admins[i].user, user);
+        strcpy(admins[i].pass, pass);
+    }
+    fclose(fad);
+    char myuser[20];
+    char mypass[20];
+    printf("Enter user: ");
+    scanf("%19s", myuser);
+    printf("Enter pass: ");
+    scanf("%19s", mypass);
+    for (int i = 0; i < noOfAdmins; i++)
+    {
+        if (strcmp(admins[i].user, myuser) == 0 && strcmp(admins[i].pass, mypass) == 0)
+        {
+            return 1;
+        }
+        
+    }
+    // printf("Incorrect user or pass.\n");
+    printf("Incorrect user or password.\n");
+    sleep(2);
+    system("cls");
+    return 0;
+    
+    
+}
+
+int main() {
+    int f = 0;
+    while (f == 0)
+    {
+        f = login();
+    }
+    system("cls");
+    printf("Hello Admin.\n");
+    int choice = 0;
+    while (choice != 4)
+    {
+        printf("Enter 1 to add new items to stock.\nEnter 2 to add a new employee.\nEnter 3 to add a new Admin.\nEnter 4 to exit and sign out.");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            newItem();
+            break;
+        case 2:
+            /* code */
+            break;
+        case 3:
+            /* code */
+            break;    
+        default:
+            break;
+        }
+    }
+    
+
+    return 0;
+}
